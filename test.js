f351cec960282fcbb98b930bd48480b4c0e7e540
AV8SR <australianborderroleplay@gmail.com>
2025-08-16 20:02:48 +1000

    Create test.js

---
+++ test.js
@@ -0,0 +1,52 @@
+{
+  "flags": 32768,
+  "components": [
+    {
+      "type": 17,
+      "components": [
+        {
+          "type": 12,
+          "items": [
+            {
+              "media": {
+                "url": "https://media.discordapp.net/attachments/1403864768879657064/1404368541137768468/Screenshot_2025-08-11_at_5.39.07_pm.png?ex=68a18720&is=68a035a0&hm=8484b40cf34b9b9fa906d852452c91c8c951ce76ea704c87be1fdd823b56e1da&=&format=webp&quality=lossless&width=1100&height=398"
+              }
+            }
+          ]
+        },
+        {
+          "type": 9,
+          "components": [
+            {
+              "type": 10,
+              "content": "### <:CCRP:1403548211783798815> Session Start Up! <:CCRP:1403548211783798815> \nG'day all Central Coast Roleplay members, the staff team here at CCRP have initiated a session! Please join up to get the roleplaying started, note that we are always looking at hiring staff, interested in being a staff member? Head over to <#1396051805812817960> after or during the session to complete the form.\n\n\nIf you voted you ***MUST*** join the session within 10 minutes to avoid punishments/infractions."
+            }
+          ],
+          "accessory": {
+            "type": 2,
+            "style": 5,
+            "label": "Quick Join",
+            "emoji": {
+              "id": "1397124996366598198",
+              "name": "unknown",
+              "animated": false
+            },
+            "url": "roblox://placeId=2534724415&launchData=%7B%22psCode%22%3A%22centralcrp%22%7D",
+            "custom_id": "p_203812506366382082"
+          }
+        },
+        {
+          "type": 12,
+          "items": [
+            {
+              "media": {
+                "url": "https://media.discordapp.net/attachments/1368117364583039067/1405430913730609202/Screenshot_2025-08-14_at_4.00.33_pm.png?ex=689ecd09&is=689d7b89&hm=22013ef3f63b714eacae7de6bc9c4bcf8fe592bcda21e9278d841b057d81f35e&=&format=webp&quality=lossless&width=3324&height=46"
+              }
+            }
+          ]
+        }
+      ],
+      "accent_color": 16777215
+    }
+  ]
+}
